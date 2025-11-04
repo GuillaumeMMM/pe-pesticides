8fd5d09f83d9c0e8a4ffb81608004ffb1ec5005b
GuillaumeMMM <guillaume.meigniez@gmail.com>
2025-11-04 14:09:36 +0100

    Fix typo.

--- src/components/sources.svelte
+++ src/components/sources.svelte
@@ -11,7 +11,7 @@
 	received from the European Chemicals Agency (ECHA) and national authorities. Figures given in export
 	notifications are estimates issued by companies before the first annual export takes plance and may
 	differ from the final amount actually exported. Only pesticides banned in the European Union (EU) as
-	'plant protection products' and identified ad exported for crop protection are included here. In most
+	'plant protection products' and identified as exported for crop protection are included here. In most
 	cases, figures used here are for mixtures containung banned chemicals, not the banned chemicals alone.
 </p>
 
