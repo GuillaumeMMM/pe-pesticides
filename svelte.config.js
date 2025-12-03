a91f0691d94fdf62f425859e78369cb00a775713
GuillaumeMMM <guillaume.meigniez@gmail.com>
2025-12-03 11:54:46 +0100

    Fix build.

--- svelte.config.js
+++ svelte.config.js
@@ -8,18 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: true,
-			strict: true
-		})
+		adapter: adapter({ strict: false }),
+		prerender: { entries: ['*'] },
+		paths: { base: '', relative: true }
 	}
 };
 
